8f6f5cb673d947237dd4079d650eaff2bcbcf4f7
aeldenburg <aeldenburg@gmail.com>
2019-08-25 12:20:49 -0700

    fixed class name on Team page

--- camper/src/components/Team/team.js
+++ camper/src/components/Team/team.js
@@ -9,70 +9,70 @@ function Team() {
       <hr className="divider my-4"></hr>
       <div className="row">
 
-      <div class="container">
-        <div class="row">
-            <div class="col-sm">
-            <img src="https://images.unsplash.com/photo-1504812533430-d76f0058aff2?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="side-img1" alt="code"/>
+      <div className="container">
+        <div className="row">
+            <div className="col-sm">
+            <img src="https://images.unsplash.com/photo-1504812533430-d76f0058aff2?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="side-img1" alt="code"/>
             </div>
-            <div class="col-md">
-            <div class="bd-example">
-                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <div className="col-md">
+            <div className="bd-example">
+                <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
                     
-                    <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="https://media.licdn.com/dms/image/C5603AQHwQBmqq-wgbg/profile-displayphoto-shrink_800_800/0?e=1572480000&v=beta&t=H1vv7wawCLfufu9vzZmqaAx4Rw_uT1Ie2uOfgXhsceo" class="d-block w-100" alt="eric"/>
-                        <div class="carousel-caption d-none d-md-block">
+                    <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://media.licdn.com/dms/image/C5603AQHwQBmqq-wgbg/profile-displayphoto-shrink_800_800/0?e=1572480000&v=beta&t=H1vv7wawCLfufu9vzZmqaAx4Rw_uT1Ie2uOfgXhsceo" className="d-block w-100" alt="eric"/>
+                        <div className="carousel-caption d-none d-md-block">
                         </div>
                         <br></br>
                         <h5>Eric Schubert</h5>
                         <p>Bio goes here.</p>
                     </div>
-                    <div class="carousel-item">
-                        <img src="https://avatars2.githubusercontent.com/u/48971375?s=460&v=4" class="d-block w-100" alt="andrew"/>
-                        <div class="carousel-caption d-none d-md-block">
+                    <div className="carousel-item">
+                        <img src="https://avatars2.githubusercontent.com/u/48971375?s=460&v=4" className="d-block w-100" alt="andrew"/>
+                        <div className="carousel-caption d-none d-md-block">
                         </div>
                         <br></br>
                         <h5>Andrew Eldenburg</h5>
                         <p>Full stack web developer with a passion for making even the most complex things simple to understand and easy to use.</p>
                     </div>
-                    <div class="carousel-item">
-                        <img src="https://avatars2.githubusercontent.com/u/48640035?s=460&v=4" class="d-block w-100" alt="camerone"/>
-                        <div class="carousel-caption d-none d-md-block">
+                    <div className="carousel-item">
+                        <img src="https://avatars2.githubusercontent.com/u/48640035?s=460&v=4" className="d-block w-100" alt="camerone"/>
+                        <div className="carousel-caption d-none d-md-block">
                         </div>
                         <br></br>
                         <h5>Camerone Stoney</h5>
                         <p>Full-Stack Web Developer, experienced supervisor and creative thinker.</p>
                     </div>
-                    <div class="carousel-item">
-                        <img src="https://media.licdn.com/dms/image/C5603AQFDEDVj1tRd8A/profile-displayphoto-shrink_800_800/0?e=1572480000&v=beta&t=LPVeMCfkVSwrwu1d6aNWUt0oR1k2RjN2fbMJ6vb5QLk" class="d-block w-100" alt="cruz"/>
-                        <div class="carousel-caption d-none d-md-block">
+                    <div className="carousel-item">
+                        <img src="https://media.licdn.com/dms/image/C5603AQFDEDVj1tRd8A/profile-displayphoto-shrink_800_800/0?e=1572480000&v=beta&t=LPVeMCfkVSwrwu1d6aNWUt0oR1k2RjN2fbMJ6vb5QLk" className="d-block w-100" alt="cruz"/>
+                        <div className="carousel-caption d-none d-md-block">
                         </div>
                         <br></br>
                         <h5>Cruz Morgan</h5>
                         <p>Bio goes here.</p>
                     </div>
-                    <div class="carousel-item">
-                        <img src="https://avatars1.githubusercontent.com/u/17563089?s=460&v=4" class="d-block w-100" alt="sam"/>
-                        <div class="carousel-caption d-none d-md-block">
+                    <div className="carousel-item">
+                        <img src="https://avatars1.githubusercontent.com/u/17563089?s=460&v=4" className="d-block w-100" alt="sam"/>
+                        <div className="carousel-caption d-none d-md-block">
                         </div>
                         <br></br>
                         <h5>Samantha Lossett</h5>
                         <p>I am currently working towards my certificate in Full-Stack web development at the University of Oregon.</p>
                     </div>
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
+                    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
                     </a>
-                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
+                    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
                     </a>
                 </div>
                 </div>
             </div>
-            <div class="col-sm">
-            <img src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" class="side-img2" alt="code"/>
+            <div className="col-sm">
+            <img src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" className="side-img2" alt="code"/>
             </div>
         </div>
         </div>
