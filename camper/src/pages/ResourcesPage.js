a0655429076beb6a145a704f2477eac0c9c81fe1
SamanthaLossett <samanthalossett@gmail.com>
2019-08-26 10:17:11 -0700

    added a LoginPage and a ResourcesPage, not routing yet, included the basics only

---
+++ camper/src/pages/ResourcesPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import Navbar from './components/Navbar/navbar';
+import Header from './components/Header/header';
+import Resources from './components/Resources/resources';
+import Footer from './components/Footer/footer';
+
+function ResourcesPage() {
+    return (
+        <section className="Login" id="Login">
+            <Navbar />
+            <Header />
+            <Resources />
+
+
+            <Footer />
+        </section>
+    )
+}
+export default ResourcesPage;
