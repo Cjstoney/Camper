9dc70b96469c451500348435689efefcfe8cd486
Eric Schubert <49171315+bernardericschubert@users.noreply.github.com>
2019-09-07 11:31:08 -0700

    Correcting apiRoutes path typo

--- camper/server.js
+++ camper/server.js
@@ -4,7 +4,7 @@ const path= require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./apiRoute/route');
 const db = require('./models');
 
 // middleware
