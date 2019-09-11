90b6f2af1b91993518d7dec1cadc95115d3f288f
Camerone Stoney <Cameronestoney@yahoo.com>
2019-09-11 16:45:13 -0700

    deleted a lot of boiler plate code in the routes. Get request and response works when clicking on a resource. It is not rendered to the screen yet.

--- camper/routes/apiRoutes.js
+++ camper/routes/apiRoutes.js
@@ -5,141 +5,28 @@
 const db = require("../models");
 // const sessionChecker = require("./../public/G6-HTML/js/sessionChecker");
 module.exports = function(app) {
-  // ========= CREATE ==========
-  app.get("/api/currentUser", (req, res) => {
-    res.json(req.session.user);
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
   });
-  app.post("/api/assign", function(req, res) {
-    let newAsset = req.body;
-    let newAssetAssignment = {};
-    switch (newAsset.type) {
-      case "hardware":
-        newAssetAssignment = {
-          hardware_id: newAsset.assetId,
-          user_id: newAsset.userID
-        };
-        db.UserHardware.create(newAssetAssignment).then(function(dbExample) {
-          res.json(dbExample);
-        });
-        break;
-      case "software":
-        newAssetAssignment = {
-          software_id: newAsset.assetId,
-          user_id: newAsset.userID
-        };
-        db.UserSoftware.create(newAssetAssignment).then(function(dbExample) {
-          res.json(dbExample);
-        });
-        break;
-      case "accessory":
-        newAssetAssignment = {
-          accessory_id: newAsset.assetId,
-          user_id: newAsset.userID
-        };
-        db.UserAccessory.create(newAssetAssignment).then(function(dbExample) {
-          res.json(dbExample);
-        });
-    }
-  });
-  app.post("/api/register", (req, res) => {
-    let newUser = req.body;
-    db.User.create(newUser).then(dbUser => {
-      res.json(dbUser);
-    });
-  });
-  app.post("/api/add", (req, res) => {
-    let formInfo = req.body;
-    console.log(req.body);
-    // get type of asset being added
-    switch (formInfo.assetType) {
-      case "Hardware":
-        let newHardware = {
-          manufacturer: formInfo.manufacturer,
-          asset_tag: formInfo.asset_tag,
-          serial_number: formInfo.serial_number,
-          model: formInfo.model,
-          asset_type: formInfo.assetType,
-          status: formInfo.status,
-          eol: formInfo.eol,
-          bin_location: formInfo.bin_location,
-          user_id: "1",
-          date_assigned: null
-        };
-        db.Hardware.create(newHardware).then(dbUser => {
-          res.json(dbUser);
-        });
-        break;
-      case "Software":
-        let newSoftware = {
-          manufacturer: formInfo.manufacturer,
-          product: formInfo.product,
-          product_key: formInfo.product_key,
-          expiration_date: formInfo.expiration_date,
-          license_size: formInfo.license_size,
-          license_available: formInfo.license_available
-        };
-        db.Software.create(newSoftware).then(dbUser => {
-          res.json(dbUser);
-        });
-        break;
-      case "Accessory":
-        let newAccessory = {
-          manufacturer: formInfo.manufacturer,
-          model: formInfo.model,
-          accessory_type: formInfo.accessory_type,
-          quantity_available: formInfo.accessory_type,
-          bin_location: formInfo.bin_location
-        };
-        db.Accessory.create(newAccessory).then(dbUser => {
-          res.json(dbUser);
-        });
-        break;
-      default:
-        console.log("No assets added");
-    }
-  });
-  app.post("/api/request", (req, res) => {
-    let newRequest = req.body;
-    console.log(newRequest);
-    db.Requests.create(newRequest).then(dbUser => {
-      res.json(dbUser);
-    });
-  });
-  app
-    .route("/api/login")
-    .get(sessionChecker, (req, res) => {
-      res.json("/");
-    })
-    .post((req, res) => {
-      let username = req.body.username;
-      let password = req.body.password;
-      db.User.findOne({ where: { username: username } }).then(function(user) {
-        if (!user) {
-          res.json("/");
-        } else if (user.password !== password) {
-          res.json("/");
-        } else {
-          req.session.user = user.dataValues;
-          console.log(req.session.user);
-          if (user.role === "1") {
-            res.json("/user");
-          } else {
-            res.json("/admin");
-          }
-        }
-      });
+  
       // =========== READ ==============
       // Get all 
+      app.get('/', function (req, res) {
+        res.send('Hello World');
+      });
       app.get("/api/all-resources", function(req, res) {
-        db.Resouce.findAll().then(function(results) {
+        db.Resource.findAll().then(function(results) {
           res.json(results);
         });
       });
       // ========= Get Specific Type Of Resource ==========
       app.get("/api/resource/:type", function(req, res) {
-        db.Resource.findAll({ where: { type: req.params.type } }).then(function(
+        db.Resource.findAll({ where: { technology: req.params.type } }).then(function(
           results
-        ) {
+          ) {
+          console.log(res);
           res.json(results);
         });
       });
@@ -242,5 +129,4 @@ module.exports = function(app) {
           res.json(dbExample);
         });
       });
-    });
-};
+    };
--- camper/src/pages/ResourceTypePage.js
+++ camper/src/pages/ResourceTypePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from '../components/Navbar/navbar';
-import Cards from "../components/Cards/Cards"
+import {Card, Button} from 'react-bootstrap/Card';
 import Footer from '../components/Footer/footer';
 import axios from 'axios';
 
@@ -11,31 +11,40 @@ class ResourceTypePage extends React.Component {
     state={
         post: null
     }
-
+    
     componentDidMount(){
         let id = this.props.match.params.ResourceTypePage;
         console.log(id)
-        axios.get("/api/resource/:"+id)
-            .then(res =>{
-                this.setState({
-                    post: res.data
+        axios.get("http://localhost:3001/api/resource/"+id)
+        .then(res =>{
+            this.setState({
+                post: res.data
             })
-            console.log(res)
+            console.log(this.state.post)
+            // console.log(res.data)
         })
-
-
-    }
-
-
+    } 
+    renderCards = () => {    
+        return this.state.post.map(resource =>
+        
+            <Card style={{ width: '10rem', margin: '100px 0px 20px 50px' }}>
+          <Card.Img style={{maxHeight: '200px'}} variant="top" src={resource.technology} />
+          <Card.Body>
+            <Card.Title>{resource.technology}</Card.Title>
+            <Card.Text>{resource.description}</Card.Text>
+            <Button variant="primary" href={resource.url}>Learn It!</Button>
+          </Card.Body>
+        </Card>
+        )}
     render() {
 
 
         return (
             <section className="ResourcesClass" id="ResourcesClass">
                 <Navbar />
-                
-                {/* <h1 style={{margin: '100px'}}>{this.state.id}</h1> */}
-                {/* <Cards /> */}
+                <div>
+                {this.renderCards()}
+                </div>
                 <Footer />
             </section>
         )
