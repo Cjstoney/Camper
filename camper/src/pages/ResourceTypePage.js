4e77c485165c074dcbfaa511ad0b4f996ad5ef16
Camerone Stoney <Cameronestoney@yahoo.com>
2019-09-11 21:53:36 -0700

    content renders unformatted and links to resource page

--- camper/src/pages/ResourceTypePage.js
+++ camper/src/pages/ResourceTypePage.js
@@ -10,41 +10,51 @@ import axios from 'axios';
 class ResourceTypePage extends React.Component {
     constructor() {
         super()
-            this.state = {
-                post: null
-            }
-        }
-    
-
-        componentDidMount(){
-            let id = this.props.match.params.ResourceTypePage;
-            console.log(id)
-            axios.get("http://localhost:3001/api/resource/" + id)
-                .then(res => {
-                    this.setState({
-                        post: res.data
-                    })
-                    console.log(this.state.post)
-                    // console.log(res.data)
-                })
-        }
-        render() {
-            // console.log(this.state.post.technology)
-            const post = this.state.post ? (
-                <div className="post">
-                   { <h1 style={{margin: '100px'}}>{this.state.post[0].technology}</h1>}
-                </div>
-            ) : (
-                    <h1 >nothing to see here</h1>)
-
-            return (
-                <section className="ResourcesClass" id="ResourcesClass">
-                    <Navbar />
-                    <div>{post}</div>
-                    <Footer />
-                </section>
-            )
+        this.state = {
+            post: null
         }
     }
+
+
+    componentDidMount() {
+        let id = this.props.match.params.ResourceTypePage;
+        console.log(id)
+        axios.get("http://localhost:3001/api/resource/" + id)
+            .then(res => {
+                this.setState({
+                    post: res.data
+                })
+                console.log(this.state.post)
+                // console.log(res.data)
+            })
+    }
+    render() {
+        // console.log(this.state.post.technology)
+        const post = this.state.post ? (
+            <div className="post">
+                {
+                    <div style={{ margin: '100px' }}>
+                        <a href={this.state.post[0].url}><h1>{this.state.post[0].technology}</h1>
+                            <h2>{this.state.post[0].description}</h2>
+                        </a>
+                    </div>
+                }
+            </div>
+        ) : (
+                <h1 >No resource listed at this point and time</h1>)
+
+        return (
+            <section className="ResourcesClass" id="ResourcesClass">
+                <Navbar />
+                <div>
+                    {
+                        post
+                    }
+                </div>
+                <Footer />
+            </section>
+        )
+    }
+}
 
 export default ResourceTypePage;
