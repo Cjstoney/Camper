d9b6f96d7daadda4a94c8999afbc14374056c44e
SamanthaLossett <samanthalossett@gmail.com>
2019-09-16 16:36:16 -0700

    added local storage to login page

--- camper/src/components/Login/Login.js
+++ camper/src/components/Login/Login.js
@@ -4,30 +4,77 @@ import Button from 'react-bootstrap/Button';
 import "./style.css";
 
 class Login extends React.Component {
-    render() {
-      return (
-        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-  <Button style={{ marginLeft: '10px'}} variant="primary" type="submit" href="/signup">
-    Or Sign Up
-  </Button>
-</Form>
-   
-)
-    };
+  AssignValues = () => {
+    console.log("input from login form: ");
+    localStorage.setItem('emailLogin', document.form1.emailLogin.value);
+    localStorage.setItem('pwLogin',  document.form1.pwLogin.value);
+    
   }
-  export default Login;
+Value1= () =>
+  { 
+      console.log("Value of emailLogin is: " + localStorage.getItem("emailLogin")); 
+      }
+
+ Value2= ()=>
+  {
+       console.log("Value of pwLogin is: " + localStorage.getItem("pwLogin")); 
+       }
+
+  ShowLength = ()=>{
+    console.log("num of items in local storage is: " + localStorage.length);
+  }
+
+  render() {
+    return (
+
+      <Form name="form1">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control input='true' type="email" name="emailLogin" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+    </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name="pwLogin" placeholder="Password" />
+        </Form.Group>
+
+
+        <Button onClick={this.AssignValues} className="submitButton" variant="primary" type="submit">
+          Submit
+  </Button>
+
+        <Button style={{ margin: '20px 20px 20px 20px' }} variant="primary" type="submit" href="/signup">
+          Or Sign Up
+  </Button>
+      </Form>
+
+    )
+  };
+}
+export default Login;
+
+                              //try #1 //
+// handleClick= () =>{
+//   alert("this is the handleClick working sam");
+
+//   let email1=document.getElementById('emailLogin');
+//   let pw=document.getElementById('pwLogin');
+
+//   function store (){
+//     localStorage.setItem('emailLogin', email1.value);
+//     localStorage.setItem('pwLogin', pw.value);
+//     console.log("input from login form: ", email1, pw);
+//   }
+
+//   function check(){
+//     let storedName= localStorage.getItem('emailLogin');
+//     let storedPw= localStorage.getItem('pwLogin');
+//     console.log("stored data from form: ", storedName, storedPw );
+//   }
+//   store();
+//   check();
+// }
+                              //try #2 //
