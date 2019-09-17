521dc0d1e3d50c82dbd97a275347158ce3261746
Camerone Stoney <Cameronestoney@yahoo.com>
2019-09-17 12:10:30 -0700

    saved page mostly built. need to fix a few things with the double check that user is logged in.

--- camper/src/pages/SavedPage.js
+++ camper/src/pages/SavedPage.js
@@ -1,30 +1,74 @@
 import React from "react";
 import Navbar from '../components/Navbar/navbar';
 import Footer from "../components/Footer/footer";
+import Axios from "axios";
 // import Card from 'react-bootstrap/Card';
 // import Button from 'react-bootstrap/Button';
 
-// import axios from 'axios';
+import axios from 'axios';
 
 class SavedPage extends React.Component{
     constructor(){
         super()
         this.state={
-            resouce: null
+            resource: null,
+            auth: false
         }
     }
 
+    componentDidMount(){
+        axios.get("/api/resources/saved")
+        .then(res=>{
+            this.setState({
+                resource: res.data
+            })
+        })
+        }
 
-    return(
-        <div  className="savedPageDiv">
+        render(){
+            console.log(this.state.resource)
+            let resource = this.state.resource ? ((
+                <div className="resource">
+                    {
+                        this.state.resource.map(function (e, idx, arr) {
+                            console.log(e)
+                            
+                            return (
+                                
+                                
+                                <Card className="mainCard" key={e.id}style={{ width: '75%'}}>
+                                    {/* <Card.Img style={{maxHeight: '200px'}} variant="top" src={resource.img} /> */}
+                                    <Card.Body>
+                                    <Card.Img  style={{maxWidth:"100px"}} variant="top" src={e.imgurl} />
+                                        <Card.Title>{e.technology}</Card.Title>
+                                        <Card.Text>{e.description}</Card.Text>
+                                        <Button variant="primary" href={e.url}>Learn It</Button>
+                                        <Button style={{marginLeft:"10px"}} variant="primary" href={e.url}>Save</Button>
+                                    </Card.Body>
+                                </Card>
+                                
+    
+                            )
+                        })
+                    }
+                </div>
+            ) ):(
+                <h1>You have not saved anyresources yet.</h1>
+            )
+            
+            return(
+                <div  className="savedPageDiv">
         <Navbar />
 
             <h1 style={{marginTop: '100px'}}>Your Saved Resources</h1>
             <br></br>
-            <h3>(the saved cards [] [] []  will render here)</h3>
+            {
+                resource
+            }
         <Footer />
         </div>
     );
+}
 }
 
 export default SavedPage;
