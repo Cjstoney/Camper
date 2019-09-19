b33be64af2c41ba04ad7959bf66fff31ff93c5fa
aeldenburg <aeldenburg@gmail.com>
2019-09-19 16:50:18 -0700

    fixed Login page formatting

--- camper/src/components/Login/Login.js
+++ camper/src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import "./style.css";
+import "./login.css";
 import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
