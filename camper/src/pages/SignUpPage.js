9977955189442cc05fb4b2d71dae91f73e95efe0
aeldenburg <aeldenburg@gmail.com>
2019-09-19 19:27:53 -0700

    tweaks to Login and SignUp pages

--- camper/src/pages/SignUpPage.js
+++ camper/src/pages/SignUpPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Signup from "../components/Signup/signup";
-import Navbar from "../components/Navbar/navbar";
 // import Footer from '../components/Footer/footer';
 
 
@@ -8,9 +7,7 @@ import Navbar from "../components/Navbar/navbar";
 function SignupPage(){
     return(
         <section className="signupdiv">
-            <Navbar />
             <Signup />
-            {/* <Footer /> */}
         </section>
     )
 }
